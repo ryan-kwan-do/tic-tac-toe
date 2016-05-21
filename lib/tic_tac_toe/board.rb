d10da9378dcff8a56bbb0fa66fcdd0124c0f07ee
Ryan Armstrong <hello@ryanarmstrong.me>
2016-05-21 16:58:58 +0200

    added get and set methods for square

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -2,12 +2,23 @@ module TicTacToe
   class Board
   #board is initialized with the hash :grid	
   	attr_reader :grid
-  	def initialize(input)
-  	  @grid = input.fetch(:grid)	
+  	def initialize(input={})
+  	  @grid = input.fetch(:grid, default_grid)	
   	end
 
+    def get_cell(x,y)
+      grid[x][y]
+    end
+
+#set cell, dependent on the square class!!!
+    def set_cell(x,y, value)
+      get_cell(x,y).value = value
+    end
+
+    private
+
   	def default_grid
-  	  Array.new(3) { Array.new(3) {Cell.new }}
+  	  Array.new(3) { Array.new(3) {Square.new }}
   	end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,7 +7,45 @@ module TicTacToe
   	  it "has a grid key" do
   	  	expect {Board.new(grid: "grid")}.to_not raise_error
   	  end
+
+  	  it "sets the grid with three rows by default" do
+  	  	board = Board.new
+  	  	expect(board.grid.size).to eql(3)
+  	  end
+
+  	  it "creates three things in each row by default" do
+  	  	board = Board.new
+  	  	board.grid.each do |row|
+  	  	  expect(row.size).to eql(3)
+  	    end
+  	  end
+  	end  
+  	
+  	context "#grid" do
+  	  it "returns the grid" do
+  	    board = Board.new(grid: "Blah")
+  	    expect(board.grid).to eql "Blah"
+  	  end
   	end
+
+  	context "get square" do
+  	  it "returns the right square based on the entered x,y coordinate" do
+  	  	grid = [["", "", ""], ["", "", "return_me"], ["", "", ""]]
+  	  	board = Board.new(grid: grid)
+  	  	expect(board.get_cell(1,2)).to eql("return_me")
+  	  end
+  	end
+
+  	context	"#set_cell" do
+  	  it "updates the value of the cell at the x,y coordinate" do
+  	  	Bob = Struct.new(:value)
+  	  	grid = [[Bob.new("pickle"), "", ""], ["", "", ""], ["", "", ""]]
+  	  	board = Board.new(grid: grid)
+  	  	board.set_cell(0,0,"burger")
+  	  	expect(board.get_cell(0,0).value).to eql("burger")
+  	  end
+  	end
+
   end
 
 end
