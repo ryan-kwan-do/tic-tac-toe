6960a87318004a2a1345fc75496fac399de2919c
Ryan Armstrong <hello@ryanarmstrong.me>
2016-05-22 13:56:42 +0200

    finished board logic

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -27,5 +27,34 @@ module TicTacToe
   	  Array.new(3) { Array.new(3) {Square.new }}
   	end
 
+    def draw?
+      grid.flatten.map {|square| square.value}.none_empty?
+    end
+
+    def winner?
+      winning_positions.each do |winning_position|
+        next if winning_position_values(winning_position).all_empty?
+        return true if winning_position_values(winning_position).all_same?
+      end
+      false
+    end    
+
+    def winning_positions
+      grid + # rows
+      grid.transpose + # columns
+      diagonals
+    end
+
+    def diagonals
+      [
+        [get_cell(0,0), get_cell(1,1), get_cell(2,2)],
+        [get_cell(0,2),get_cell(1,1), get_cell(2,0)]
+      ]
+    end
+
+    def winning_position_values(winning_position)
+      winning_position.map { |square| square.value}
+    end
+
   end
 end
